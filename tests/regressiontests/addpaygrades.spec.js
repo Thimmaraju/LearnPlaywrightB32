58f804248ccfb06b55939b6d5a68771e8646497b
G Thimmaraju <thimmaraju418@gmail.com>
2025-11-03 09:36:12 +0530

    data from Json

--- tests/regressiontests/addpaygrades.spec.js
+++ tests/regressiontests/addpaygrades.spec.js
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+import logindata from "../../testdata/logindata.json"
+
 test('Verify Add Pay grades', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Username' }).fill(logindata.username);
 
   await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
